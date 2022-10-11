bd075f08ade4f6d564b1d4e9e4271b47ede65c9f
Karsten Vermeulen <Karsten.vermeulen.dev@gmail.com>
2022-10-11 15:21:21 +0100

    Add Lesson 3 code

---
+++ Lesson 3/Graphics_Engine/Main.cpp
@@ -0,0 +1,74 @@
+#include <Windows.h>
+#include <gl/GL.h>
+
+#include <iostream>
+#include <SDL.h>
+
+bool isAppRunning = true;
+SDL_Window* window = nullptr;
+SDL_GLContext context = nullptr;
+
+int main(int argc, char* argv[])
+{
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) == -1)
+	{
+		std::cout << "Error initializing SDL" << std::endl;
+	}
+
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+	
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	
+	//SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
+	
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+
+	window = SDL_CreateWindow("Graphics Engine", 
+							  SDL_WINDOWPOS_UNDEFINED, 
+							  SDL_WINDOWPOS_UNDEFINED, 
+							  1280, 720, SDL_WINDOW_OPENGL);
+
+	if (!window)
+	{
+		std::cout << "Error creating SDL window" << std::endl;
+	}
+
+	context = SDL_GL_CreateContext(window);
+
+	if (!context)
+	{
+		std::cout << "Error creating OpenGL context." << std::endl;
+	}
+
+	//=================================================================
+
+	while (isAppRunning)
+	{
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//update/render stuff
+
+		SDL_GL_SwapWindow(window);
+
+	}
+
+	//=================================================================
+
+	SDL_GL_DeleteContext(context);
+
+	SDL_DestroyWindow(window);
+
+	SDL_Quit();
+
+	system("pause");
+	return 0;
+}
