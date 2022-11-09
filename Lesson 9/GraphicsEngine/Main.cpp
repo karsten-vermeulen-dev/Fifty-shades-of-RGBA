57bea0ceca0444220ac5579b5af7c27f748d0215
Karsten Vermeulen <Karsten.vermeulen.dev@gmail.com>
2022-11-09 10:57:29 +0000

    Add multiple VBOs, EBO and VAO

--- Lesson 9/GraphicsEngine/Main.cpp
+++ Lesson 9/GraphicsEngine/Main.cpp
@@ -9,7 +9,10 @@
 Shader shader;
 bool isAppRunning{ true };
 
-GLuint VBO{ 0 };
+GLuint VAO{ 0 };
+GLuint EBO{ 0 };
+GLuint vertexVBO{ 0 };
+GLuint colorVBO{ 0 };
 
 int main(int argc, char* argv[])
 {
@@ -27,25 +30,43 @@ int main(int argc, char* argv[])
 	shader.Create("Shaders/Main.vert", "Shaders/Main.frag");
 	shader.Use();
 
-	GLfloat VBOData[] = { -0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-						   0.5f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
-						  -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f,
-						  -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f,
-						   0.5f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
-						   0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f };
+	GLfloat vertices[] = { -0.5f,  0.5f, 0.0f,
+							0.5f,  0.5f, 0.0f,
+							0.5f, -0.5f, 0.0f,
+						   -0.5f, -0.5f, 0.0f };
+
+	GLfloat colors[] = { 1.0f, 0.0f, 0.0f,
+						 0.0f, 0.0f, 1.0f,
+						 0.0f, 1.0f, 0.0f,
+						 0.0f, 1.0f, 1.0f };
+
+	GLuint indices[] = { 0, 1, 3,
+						 3, 1, 2 };
 
 	auto vertexAttributeID = shader.GetAttributeID("vertexIn");
 	auto colorAttributeID = shader.GetAttributeID("colorIn");
 
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(VBOData), VBOData, GL_STATIC_DRAW);
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &vertexVBO);
+	glGenBuffers(1, &colorVBO);
+	glGenBuffers(1, &EBO);
 
-	glVertexAttribPointer(vertexAttributeID, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), 0);
-	glVertexAttribPointer(colorAttributeID, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glBindVertexArray(VAO);
 
+	glBindBuffer(GL_ARRAY_BUFFER, vertexVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(vertexAttributeID, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(vertexAttributeID);
+
+	glBindBuffer(GL_ARRAY_BUFFER, colorVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+	glVertexAttribPointer(colorAttributeID, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(colorAttributeID);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glBindVertexArray(0);
 
 	while (isAppRunning)
 	{
@@ -58,12 +79,18 @@ int main(int argc, char* argv[])
 			isAppRunning = false;
 		}
 
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 
 		Screen::Instance()->Present();
 	}
 
-	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+	glDeleteBuffers(1, &colorVBO);
+	glDeleteBuffers(1, &vertexVBO);
+	glDeleteVertexArrays(1, &VAO);
+
 	glDisableVertexAttribArray(colorAttributeID);
 	glDisableVertexAttribArray(vertexAttributeID);
 
